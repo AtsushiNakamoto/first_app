dcdb941515cb8c8e438b5a55c34db180151351e9
EC2 Default User <ec2-user@ip-172-31-24-48.ap-northeast-1.compute.internal>
2021-06-05 05:33:59 +0000

    2回目のコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+    
     # get URLパターン, to; ,コントローラ名$アクション名
+    # Edit 2021-06-05
     get 'posts', to: 'posts#index'
     get 'posts/new', to: 'posts#new'
     post 'posts', to: 'posts#create'
+
 end
